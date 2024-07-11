149e646d35e40d53b3c8007dd92776fa6c260a70
Raviteja1652 <ravitejamrt.165@gmail.com>
2024-07-11 10:39:43 +0530

    modified date and added css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+    const month = props.date.toLocaleString('en-US', {month: 'long'})
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'})
+    const year = props.date.getFullYear()
+
     return (
         <div className="expense-item">
-            <div>
-                {props.date.toISOString()}
+            <div className='expense-date'>
+                <div className='expense-date__month'>{month}</div>
+                <div className='expense-date__day'>{day}</div>
+                <div className='expense-date__year'>{year}</div>
             </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
